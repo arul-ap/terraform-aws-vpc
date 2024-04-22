4ef590ae1cc63fcdd74f700fab08c32158fc4798
Muthulingam Arultharan <arul@asiapac.com.sg>
2024-04-22 17:26:26 +0800

    added sample config

--- modules/tgw_subnets/outputs.tf
+++ modules/tgw_subnets/outputs.tf
@@ -1,5 +1,5 @@
 
 output "tgw_subnet_id" {
-  description = "Transit Gateway Subnet ID"
-  value = {for k,v in aws_subnet.tgw_subnets: k => aws_subnet.tgw_subnets[k].id}
+  description = "Transit Gateway Subnet ID list"
+  value = [for k,v in aws_subnet.tgw_subnets: aws_subnet.tgw_subnets[k].id]
 }
