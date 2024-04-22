5297038234034a92e861a2db656ee2d7dea7fd32
Muthulingam Arultharan <arul@asiapac.com.sg>
2024-04-23 06:46:28 +0800

    formatting

--- examples/sample/outputs.tf
+++ examples/sample/outputs.tf
@@ -1,66 +1,66 @@
 
 output "vpc_id" {
   description = "VPC ID"
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
 }
 
 output "igw_id" {
   description = "Internet Gateway ID"
-  value = module.vpc.igw_id
+  value       = module.vpc.igw_id
 }
 output "public_subnet_id" {
   description = "Public Subnet ID"
-  value = module.vpc.public_subnet_id
+  value       = module.vpc.public_subnet_id
 }
 output "private_subnet_id" {
   description = "Private subnet ID"
-  value = module.vpc.private_subnet_id
+  value       = module.vpc.private_subnet_id
 }
 
 output "natgw_id" {
   description = "NAT gateway ID"
-  value = module.vpc.natgw_id
+  value       = module.vpc.natgw_id
 }
 output "natgw_eip_id" {
   description = "Elastic IP Allocation ID for NAT gateway"
-  value = module.vpc.natgw_iep_id
+  value       = module.vpc.natgw_iep_id
 }
 
 
 output "default_nacl_id" {
   description = "Default NACL ID"
-  value = module.vpc.default_nacl_id
+  value       = module.vpc.default_nacl_id
 }
 
 
 output "nacl_id" {
   description = "NACL ID"
-  value = module.vpc.nacl_id
-  
+  value       = module.vpc.nacl_id
+
 }
 output "default_sg_id" {
   description = "Default Security Group ID"
-  value = module.vpc.default_sg_id
+  value       = module.vpc.default_sg_id
 }
 
 
- output "sg_id" {
+output "sg_id" {
   description = "Security Group ID"
-   value = module.vpc.sg_id
- }
+  value       = module.vpc.sg_id
+}
 
 
 output "rt_id" {
   description = "Route Tables ID"
-  value = module.vpc.rt_id
+  value       = module.vpc.rt_id
 }
 
 output "tgw_subnet_id" {
   description = "Transit Gateway attachment ID"
-  value = module.vpc.tgw_subnet_id
+  value       = module.vpc.tgw_subnet_id
 }
 
 output "tgw_attachment_id" {
   description = "Transit Gateway Attachment ID"
-  value = module.vpc.tgw_attachment_id
+  value       = module.vpc.tgw_attachment_id
 }
--- examples/sample/variables.tf
+++ examples/sample/variables.tf
@@ -1,9 +1,3 @@
-#variables
-variable "region" {
-  type    = string
-  default = "ap-southeast-1"
-}
-
 variable "default_tags" {
   description = "AWS account level tags inherit to all resources"
   type        = map(string)
